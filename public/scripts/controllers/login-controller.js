04d0f665d3e351f9e50abf4da3ac063b3a650c05
Chris Stanton <cstanton0760@yahoo.com>
2017-04-07 20:40:27 -0500

    login/out butons change based on status, inputs aprear and disappear basd on status, object sends to cintroller

--- public/scripts/controllers/login-controller.js
+++ public/scripts/controllers/login-controller.js
@@ -1,43 +1,54 @@
 app.controller('LoginController', ['$firebaseAuth','$http', '$location', function($firebaseAuth, $http, $location){
   var self = this;
 
-  //redirect after authentication
-  function wrapView() {
-    $location.path('/login');
-  }//wrapView()
-
-//google authenticate bellow
-  var auth = $firebaseAuth();
 //notyf must have
   // var notyf = new Notyf();
 
-//google login authentication
+//google authenticate bellow
+  var auth = $firebaseAuth();
+
+//redirection after login
+  function loginView() {
+    $location.path('/login');
+  }//loginView()
+
+//redirection after logout
+  function logoutView() {
+    $location.path('/home');
+  }//logoutView()
+
+//user google login authentication
   self.login = function(){
-    console.log("login clicked");
+        console.log("login clicked");
     auth.$signInWithPopup("google").then(function(firebaseUser) {
-      wrapView();
+      loginView();
         // notyf.confirm('You Are Logged In');
         swal("You Are Logged In", "", "success");
-        console.log("Firebase Authenticated as: ", firebaseUser.user.displayName);
-        console.log(firebaseUser.user.email)
         self.photo = firebaseUser.user.photoURL;
         self.email = firebaseUser.user.email;
+          console.log("Firebase Authenticated as: ", firebaseUser.user.displayName);
+          console.log(firebaseUser.user.email)
         //console.log("Firebase Authenticated as: ", firebaseUser.user.email);
     }).catch(function(error) {
         console.log("Authentication failed: ", error);
     });
   };//end of self.authUser()
 
-//google de-authedicate
+//user google logout de-authedicate
   self.logout = function(){
     console.log("logout clicked");
     auth.$signOut().then(function() {
+      logoutView()
         swal("You've Logged Out!", "", "success");
-        console.log('Logging the user out!');
-     });
+          console.log('Logging the user out!');
+    });
   };//end of self.deAuthUser()
 
+//new user object from view button click
+self.addNewUser = function(newUser) {
+  console.log(newUser)  
+  self.newUser = {}
+}
 
 
-
-}]);
+}]);//enf of app.controller()
