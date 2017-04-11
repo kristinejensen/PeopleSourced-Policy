c385f48c13ac8b811448c12b5e1f10406203131a
Chris Stanton <cstanton0760@yahoo.com>
2017-04-11 10:30:53 -0500

    added header controller and html view

---
+++ public/scripts/controllers/header-controller.js
@@ -0,0 +1,7 @@
+app.controller('HeaderController', ['$firebaseAuth', '$http', '$location', 'DataFactory', function($firebaseAuth, $http, $location, DataFactory){
+// var auth = $firebaseAuth();
+// var firebaseUser = auth.$getAuth();
+//   self.photo = firebaseUser.user.photoURL;
+//   self.email = firebaseUser.user.email;
+
+  }]);
