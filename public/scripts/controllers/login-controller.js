1749df821ef1998869accaa515e1abbfef5e1486
Chris Stanton <cstanton0760@yahoo.com>
2017-04-14 17:05:18 -0500

    going ot merge to development

--- public/scripts/controllers/login-controller.js
+++ public/scripts/controllers/login-controller.js
@@ -1,70 +1,70 @@
 app.controller('LoginController', ['DataFactory', '$firebaseAuth', '$http', '$location', function(DataFactory, $firebaseAuth, $http, $location){
-//CHRIS’S CODE STARTS HERE
+  //CHRIS’S CODE STARTS HERE
 
-//google authenticate bellow
+  //google authenticate bellow
   var auth = $firebaseAuth();
   var self = this;
 
-//object to verify if user exsists in DB (need to finish)
+  //object to verify if user exsists in DB (need to finish)
   var userMatchObject = DataFactory.userMatchObject.list;
 
-self.message= "hey there"
-//notyf must have
+  self.message= "hey there"
+  //notyf must have
   // var notyf = new Notyf();
 
-//redirection after login
- function loginView() {
-   $location.path('/login');
- }
-//redirection after logout
- function logoutView() {
-   $location.path('/home');
- }
-//redirection to admin view
+  //redirection after login
+  function loginView() {
+    $location.path('/login');
+  }
+  //redirection after logout
+  function logoutView() {
+    $location.path('/home');
+  }
+  //redirection to admin view
   function adminView() {
     $location.path('/admin');
   }
   var firebaseUser = auth.$getAuth();
-//user google login authentication
+  //user google login authentication
   self.login = function() {
-//call function at factory to get existing user id and email
+    //call function at factory to get existing user id and email
     DataFactory.getUserMatch();
-      auth.$signInWithPopup("google").then(function(firebaseUser) {
-//redirects to login view
-      loginView();
-//adds user google photo to view
+    auth.$signInWithPopup("google").then(function(firebaseUser) {
+      // //redirects to login view
+      // loginView();
+      //adds user google photo to view
       self.photo = firebaseUser.user.photoURL;
-//adds user google email to view
+      //adds user google email to view
       self.email = firebaseUser.user.email;
-//object contains all users
-        var userMatchObject = DataFactory.userMatchObject.list;
-//checks DB for exsisting users and then desides redirect
-          for (var i = 0; i <userMatchObject.length; i++) {
-            if (userMatchObject[i].email == firebaseUser.user.email) {
-              logoutView();
-            } else {
-              loginView();
-            }
-          };//end of for loop
-      }).catch(function(error) {
-        console.log("Authentication failed: ", error);
+      //object contains all users
+      var userMatchObject = DataFactory.userMatchObject.list;
+      //checks DB for exsisting users and then desides redirect
+      for (var i = 0; i <userMatchObject.length; i++) {
+        if (userMatchObject[i].email == firebaseUser.user.email) {
+          logoutView();
+        } else {
+          loginView();
+        }
+      };//end of for loop
+    }).catch(function(error) {
+      console.log("Authentication failed: ", error);
     });//end of .catch
   };//end of self.login()
-//user google logout de-authedicate
+  //user google logout de-authedicate
   self.logout = function() {
-          // console.log("logout clicked");
+    // console.log("logout clicked");
     auth.$signOut().then(function() {
-//redirects back to home view
+      //redirects back to home view
       logoutView();
     });//end of auth.$signOut()
   };//end of self.deAuthUser()
 
-//new user object from view button click
- self.addNewUser = function(user) {
-//brings in firebase data to function
-   var firebaseUser = auth.$getAuth();
+  //new user object from view button click
+  self.addNewUser = function(user) {
+    //brings in firebase data to function
+    var firebaseUser = auth.$getAuth();
 
-//creating a new variable with input data and firebase data
+    //creating a new variable with input data and firebase data
 
     var newUser = {
       name : firebaseUser.displayName,
@@ -73,13 +73,13 @@ self.message= "hey there"
       photo : firebaseUser.photoURL,
       word : ""
     }
-//sends object to DB
+    //sends object to DB
     DataFactory.addNewUser(newUser);
-//empties inputs after submission
+    //empties inputs after submission
     self.user = {};
-//redirects back to home view after submission
-   logoutView();
- }
+    //redirects back to home view after submission
+    logoutView();
+  }
 
-//CHRIS’S CODE ENDS HERE
+  //CHRIS’S CODE ENDS HERE
 }]);//end of app.controller()
