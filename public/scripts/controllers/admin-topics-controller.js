d5e59aa1dd5e1df10343188453a5984f7459f8a1
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-04-15 17:10:19 -0500

    Admin can update current topics.

--- public/scripts/controllers/admin-topics-controller.js
+++ public/scripts/controllers/admin-topics-controller.js
@@ -8,18 +8,11 @@ app.controller('AdminTopicsController', ['$http', '$location', function($http, $
   //                   //
   //*******************//
 
-  findActiveTopic();
+  //***************************************//
+  //         UPDATE CURRENT TOPIC          //
+  //***************************************//
 
-  function findActiveTopic(){
-    $http({
-      method:'GET',
-      url: '/admin-topics/findActiveTopic'
-    }).then(function(response){
-      console.log(response);
-      self.mainTopicTitle = response.data[0].title;
-      self.mainTopicDesc = response.data[0].description;
-    });
-  }
+  findActiveTopic();
 
   self.updateTopic = function(title, description){
     var mainTopic = {title: title, description: description}
@@ -33,11 +26,62 @@ app.controller('AdminTopicsController', ['$http', '$location', function($http, $
     })//Ends GET http
   }//Update topic function
 
+  function findActiveTopic(){
+    $http({
+      method:'GET',
+      url: '/admin-topics/findActiveTopic'
+    }).then(function(response){
+      console.log(response);
+      self.mainTopicTitle = response.data[0].title;
+      self.mainTopicDesc = response.data[0].description;
+    });
+  }
+  //***************************************//
+  //        SET NEW CURRENT TOPIC          //
+  //***************************************//
+
+
   //*******************//
   //                   //
   //    SUB TOPICS     //
   //                   //
   //*******************//
+
+  //***************************************//
+  //        UPDATE CURRENT SUBTOPICS       //
+  //***************************************//
+
+  const maxSubtopics = 5;
+
+  findActiveSubTopics();
+
+  self.updateSubTopic = function(title, description, id){
+    console.log('was clicked');
+    var subTopic = {title: title, description: description, id: id}
+    //Find the active subtopic that matches the id.
+    $http({
+      method: 'PUT',
+      url: '/admin-topics/updateActiveSubTopics',
+      data: subTopic
+    }).then(function(response) {
+      console.log(response);
+      findActiveSubTopics();
+    })//Ends GET http
+  }//Update topic function
+
+  function findActiveSubTopics(){
+    $http({
+      method:'GET',
+      url: '/admin-topics/findActiveSubTopics'
+    }).then(function(response){
+      self.subtopicsArray = response.data;
+    });
+  }
+  //***************************************//
+  //       SET NEW CURRENT SUBTOPICS       //
+  //***************************************//
+
+
 
   // var auth = $firebaseAuth();
   //
--- server/routes/admin-topics.js
+++ server/routes/admin-topics.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var pool = require('../modules/database-config');
 
+//*******************//
+//                   //
+//    MAIN TOPIC     //
+//                   //
+//*******************//
 
 router.get('/findActiveTopic', function(req, res){
   console.log('FIND admin-topics route hit');
@@ -59,5 +64,58 @@ router.put('/updateActiveTopic', function(req, res){
     });//ends client.query
   });//ends pool.connect
 });//ends router.get
+
+//*******************//
+//                   //
+//    SUB TOPICS     //
+//                   //
+//*******************//
+
+router.get('/findActiveSubTopics', function(req, res){
+  console.log('FIND admin-topics route hit');
+  var title = req.headers.title
+  var description = req.headers.description;
+  console.log(title, description);
+  pool.connect( function (err, client, done) {
+    client.query('SELECT id FROM main_topics WHERE active = true;', function(err, result){
+      done();
+      if(err){
+        console.log('Error completing find active topic query', err);
+        res.sendStatus(500);
+      } else {
+        activeMainTopicID = result.rows[0].id;
+        pool.connect(function(err, client, done){
+          client.query('SELECT * FROM subtopics WHERE main_id = $1 AND active = true ORDER BY id ASC;',
+          [activeMainTopicID], function(err, result){
+            done();
+            if(err){
+              console.log('Error completing find active topic query', err);
+              res.sendStatus(500);
+            } else {
+              res.send(result.rows);
+              console.log('here are the subtopics that are active');
+            }
+          })
+        })
+      }//ends else
+    });//ends client.query
+  });//ends pool.connect
+});//ends router.get
+
+router.put('/updateActiveSubTopics', function(req, res) {
+  var subtopic = {title: req.body.title, description: req.body.description, id: req.body.id};
+  pool.connect( function (err, client, done) {
+    client.query('UPDATE subtopics SET title = $1, description = $2 WHERE id=$3;',
+    [subtopic.title, subtopic.description, subtopic.id], function(err, result){
+      done();
+      if(err){
+        console.log('Error deactivating user', err);
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(201);
+      }
+    });
+  });
+});
 
 module.exports = router;
