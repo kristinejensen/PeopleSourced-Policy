19ff57fa93dbddb4fa5f7531543bfb01e9d267c5
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-04-17 17:42:26 -0500

    trying to merge

---
+++ public/scripts/controllers/header-controller.js
@@ -0,0 +1,6 @@
+app.controller('HeaderController', ['$firebaseAuth', '$http', '$location', 'DataFactory', function($firebaseAuth, $http, $location, DataFactory){
+
+  
+
+
+}]);//end of app.connect()
