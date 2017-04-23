cb61886e296e861ce1b132d95d37cb2471a2e853
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-04-23 16:52:18 -0500

    editing database file

--- server/psp_database.sql
+++ server/psp_database.sql
@@ -1,16 +1,32 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(80) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  address VARCHAR(2500) NOT NULL,
+  ward INTEGER,
+  admin BOOLEAN DEFAULT false,
+  active BOOLEAN DEFAULT false,
+  city VARCHAR(200),
+  state VARCHAR(200),
+  zipcode VARCHAR(200),
+  photo VARCHAR(300)
+);
 
 CREATE TABLE main_topics (
   id SERIAL PRIMARY KEY,
   title VARCHAR(80),
   description VARCHAR(10000),
-  active boolean
+  active BOOLEAN DEFAULT false,
+  upcoming BOOLEAN DEFAULT false
 );
 
 CREATE TABLE subtopics (
   id SERIAL PRIMARY KEY,
   title VARCHAR(80),
   description VARCHAR(10000),
-  main_id integer REFERENCES main_topics
+  main_id integer REFERENCES main_topics,
+  active BOOLEAN DEFAULT false,
+  upcoming BOOLEAN DEFAULT false
 );
 
 CREATE TABLE ideas (
@@ -47,7 +63,7 @@ CREATE TABLE subcomments (
   comment_id integer REFERENCES comments
 );
 
-CREATE TABLE sublikes (
+CREATE TABLE subcomments_likes (
   id SERIAL PRIMARY KEY,
   user_id integer REFERENCES users,
   subcomment_id integer REFERENCES subcomments
@@ -75,13 +91,4 @@ CREATE TABLE ideas_flags (
   id SERIAL PRIMARY KEY,
   user_id integer REFERENCES users,
   idea_id integer REFERENCES ideas
-);
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(80) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  address VARCHAR(2500) NOT NULL,
-  ward VARCHAR(80),
-  admin BOOLEAN DEFAULT false
 );
