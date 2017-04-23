d23a2cd2fa5b6e17163c8c09951f216016e468ec
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-04-23 15:37:39 -0500

    active buttons

--- public/scripts/controllers/nav-controller.js
+++ public/scripts/controllers/nav-controller.js
@@ -1,8 +1,10 @@
-app.controller('NavCtrl', ['$firebaseAuth', '$http', '$location', 'TopicsFactory', function($firebaseAuth, $http, $location, TopicsFactory){
+app.controller('NavCtrl', ['$firebaseAuth', '$http', '$location', 'TopicsFactory', '$routeParams', function($firebaseAuth, $http, $location, TopicsFactory, $routeParams){
   var self = this;
   // var self = this;
   // var auth = $firebaseAuth();
   //loads the active subtopics onto the nav.
+  self.index = $routeParams.id;
+  self.params = $location.$$url;
   TopicsFactory.findActiveSubTopics();
   //Here is the result/active subtopics
   self.subTopic = TopicsFactory.subTopic;
@@ -10,6 +12,15 @@ app.controller('NavCtrl', ['$firebaseAuth', '$http', '$location', 'TopicsFactory
   this.isActive = function (viewLocation) {
     return viewLocation === $location.path();
   };
+
+  self.redirect = function(url) {
+    if (url == 'home'){
+      console.log('hit home');
+      $location.path('/home');
+    } else {
+      $location.path('/subtopics/' + url);
+    }
+  }
 
   // TopicsFactory.checkAdminStatus();
   // self.isAdmin = TopicsFactory.isAdmin;
