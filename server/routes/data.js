9fae0975d7c2529a1dbc097d78b52ac8bccb1654
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-04-24 09:19:26 -0500

    fixing photo in comments

--- server/routes/data.js
+++ server/routes/data.js
@@ -174,7 +174,7 @@ router.get('/getComments', function(req, res) {
   console.log('zooop', ideaId);
   pool.connect()
     .then(function (client) {
-      client.query('WITH comments_likes_count_temp_table AS (SELECT comments.id AS comment_id, COUNT(comments.id) AS comments_likes_count FROM comments_likes JOIN comments ON comments_likes.comment_id=comments.id GROUP BY comments.id) SELECT comments.id AS comments_id, comments.description, comments.idea_id AS comments_idea_id, comments_likes.id AS comments_likes_id, comments_likes.user_id, comments_likes.comment_id, comments_likes_count, users.active AS user_active FROM comments LEFT OUTER JOIN comments_likes ON comments_likes.id=comments.id LEFT JOIN comments_likes_count_temp_table ON comments_likes_count_temp_table.comment_id=comments.id FULL OUTER JOIN users ON comments.user_id = users.id WHERE comments.idea_id=$1 and users.active=true;',
+      client.query('WITH comments_likes_count_temp_table AS (SELECT comments.id AS comment_id, COUNT(comments.id) AS comments_likes_count FROM comments_likes JOIN comments ON comments_likes.comment_id=comments.id GROUP BY comments.id) SELECT comments.id AS comments_id, comments.description, comments.idea_id AS comments_idea_id, comments_likes.id AS comments_likes_id, comments_likes.user_id, comments_likes.comment_id, comments_likes_count, users.active AS user_active, users.photo FROM comments LEFT OUTER JOIN comments_likes ON comments_likes.id=comments.id LEFT JOIN comments_likes_count_temp_table ON comments_likes_count_temp_table.comment_id=comments.id FULL OUTER JOIN users ON comments.user_id = users.id WHERE comments.idea_id=$1 and users.active=true;',
       [ideaId.id])
         .then(function (result) {
           client.release();
